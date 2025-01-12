f2effcf72c70796c8d8dc5175493bb19e00ec24a
adminturneddevops <mlevan1992@gmail.com>
2025-01-12 12:50:21 -0500

    KEDA

---
+++ performance-optimization/KEDA/installation/existingCluster.sh
@@ -0,0 +1,3 @@
+az aks update --resource-group your_rg_name \
+--name your_existing_cluster_name \
+--enable-keda
---
+++ performance-optimization/KEDA/installation/newCluster.sh
@@ -0,0 +1,3 @@
+az aks create --resource-group your_rg_name \
+--name your_new_cluster_name
+--enable-keda
